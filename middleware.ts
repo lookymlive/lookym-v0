3bd789e106ca9f191725d96ab242dac4caebe58b
lookymlive <lookymlive@gmail.com>
2024-10-18 21:08:17 -0300

    AWS S3 first video lookym

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export { auth as middleware } from "@/auth"
 
 // Asegúrate de que el middleware se ejecute solo en las rutas que quieres proteger
 // Todas las url que empiecen con /admin o /profile pasan por este middleware 
-
+// Aqui se incluyen las rutas que queremos proteger con el middleware de autenticación 
 export const config = {
   matcher: ["/admin/:path*", "/profile/:path*"],
 }
